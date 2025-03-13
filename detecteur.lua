34df5b72e2bad45f00715302c87c6bea07e4ecb7
batyoplantix <contact.baptistebruneaurialland@gmail.com>
2025-03-13 13:03:57 +0100

    Update detecteur.lua

--- detecteur.lua
+++ detecteur.lua
@@ -1,7 +1,6 @@
 -- On suppose que le coffre est devant la tortue
 -- Utilisation de la bibliothèque Turtle pour l'interaction avec le jeu
-
-local fs = require("fs")  -- Pour manipuler les fichiers
+ -- Pour manipuler les fichiers
 
 -- Fonction pour obtenir le nom sans namespace d'un objet
 local function getNameWithoutNamespace(objectName)
@@ -13,16 +12,16 @@ end
 -- Fonction pour interagir avec le coffre et récupérer les items
 local function getItemsFromChest()
     -- Regarder devant la tortue pour s'assurer qu'il y a un coffre
-    turtle.inspect()  -- Inspecte l'objet devant (assumant que c'est un coffre)
+    local chest = peripheral.wrap("front")
     
     -- Liste pour stocker les noms des items
     local itemNames = {}
     
     -- On prend une estimation du nombre d'items (par exemple, 27 pour un coffre standard)
-    for i = 1, 27 do
-        local success, data = turtle.getItemDetail(i)  -- Essayer de récupérer l'item
-        if success then
-            local itemName = data.name  -- Récupérer le nom de l'objet
+    for i = 1, chest.size() do
+        local item = chest.getItemDetail(i)  -- Essayer de récupérer l'item
+        if item then
+            local itemName = item.name  -- Récupérer le nom de l'objet
             table.insert(itemNames, getNameWithoutNamespace(itemName))  -- Ajouter sans namespace
         end
     end
@@ -34,7 +33,7 @@ end
 local function writeItemNamesToFile(itemNames)
     local file = fs.open("minecraftCategory.txt", "w")  -- Crée ou ouvre un fichier
     for _, name in ipairs(itemNames) do
-        file.writeLine(name.. " : 1")  -- Écrire chaque nom d'item
+        file.writeLine(name.. " : 25")  -- Écrire chaque nom d'item
     end
     file.close()  -- Fermer le fichier
 end
