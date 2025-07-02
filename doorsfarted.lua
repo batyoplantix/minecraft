bf96bd632cbfa723ac805222e1594f3bf1f05881
batyoplantix <contact.baptistebruneaurialland@gmail.com>
2025-07-02 21:23:20 +0200

    Update doorsfarted.lua

--- doorsfarted.lua
+++ doorsfarted.lua
@@ -4,7 +4,7 @@ local pos2 = {x = 20, y = 70, z = 20}
 -- Nom du joueur leader
 local leader = "lamastico"
 local url1 = "https://raw.githubusercontent.com/USER/REPO/BRANCH/fichier.dfpwm"
-local url2 = "https://raw.githubusercontent.com/USER/REPO/BRANCH/fichier.dfpwm"
+local url2 = "https://github.com/batyoplantix/minecraft/raw/refs/heads/main/Bruit2.dfpwm"
 local url3 ="https://github.com/batyoplantix/minecraft/raw/refs/heads/main/Bruit3.dfpwm"
 local url4 = "https://github.com/batyoplantix/minecraft/raw/refs/heads/main/bruit4.dfpwm"
 local speaker = peripheral.wrap("speaker_1")
